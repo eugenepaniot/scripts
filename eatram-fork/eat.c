b2eb980874e76323784fe72c5a444d7b6c187759
Eugene Paniot <e.paniot@gmail.com>
2023-11-24 16:35:47 +0000

    Update eatram-fork code

--- eatram-fork/eat.c
+++ eatram-fork/eat.c
@@ -5,22 +5,30 @@
 #include <sys/resource.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/wait.h>
+
+const int alloc_chunk = 1024 * 1024;
+
+void handle_sigint(int signum) {
+    printf("Ctrl+C exiting.\n");
+    exit(0);
+}
 
 int main( int argc, char *argv[] )
 {
-    sigset_t set;
-    char* buffer;
+    char *buffer = malloc(0);
     unsigned long long max = -1;
     unsigned long long mb = 0;
     pid_t *p;
     int i;
     int max_proc = 1;
+    int status;
 
     setvbuf(stderr, NULL, _IONBF, 0);
     setvbuf(stdout, NULL, _IONBF, 0);
 
     if(argc > 1)
-        max = atoi(argv[1]);
+        max = atoll(argv[1]);
 
     if(argc > 2)
         max_proc = atoi(argv[2]);
@@ -31,30 +39,38 @@ int main( int argc, char *argv[] )
         return 255;
     }
 
+    signal(SIGINT, handle_sigint);
+
     for (i = 0; i < max_proc; i++) {
       p[i] = fork();
 
       if(p[i]<0) {
-          perror("fork failed");
-          exit(255);
+        perror("fork failed");
+        exit(255);
       } else if (p[i] == 0) {
-          pid_t pid = getpid();
+        pid_t pid = getpid();
 
-          while((buffer=malloc(1024*1024)) != NULL && mb != max) {
-              mb++;
-              memset(buffer, 0, 1024*1024);
-              printf("%d(%d) allocated %llu MB \n", pid, i, mb);
-          }
-
+        while(mb <= max) {
+          mb++;
+          buffer = realloc(buffer, alloc_chunk * mb);
           if (buffer == NULL) {
-              perror("malloc failed");
-              exit(255);
+            perror("realloc failed");
+            break;
           }
 
-          sleep(-1);
+          memset(buffer, 0, alloc_chunk * mb);
+          printf("%d(%d) allocated %llu MB \n", pid, i, mb);
+        }
+
+        exit(0);
       }
     }
 
-    sigsuspend(&set);
+    for (i = 0; i < max_proc; i++) {
+        waitpid(p[i], &status, 0);
+    }
+
+    free(buffer);
+    free(p);
     return 0;
 }
