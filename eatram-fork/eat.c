2fcdda4774f3ed45b0cc600decd6ce0a7e4298b2
Eugene Paniot <e.paniot@gmail.com>
2023-11-27 11:48:25 +0000

    Update eatram-fork code

--- eatram-fork/eat.c
+++ eatram-fork/eat.c
@@ -2,22 +2,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
-#include <sys/resource.h>
 #include <unistd.h>
-#include <errno.h>
 #include <sys/wait.h>
 
-const int alloc_chunk = 1024 * 1024;
+
+const int alloc_chunk = 1 * 1024 * 1024;
 
 void handle_sigint(int signum) {
     printf("Ctrl+C exiting.\n");
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
 
 int main( int argc, char *argv[] )
 {
-    char *buffer = malloc(0);
-    unsigned long long max = -1;
+    unsigned long long max_mem = -1;
     unsigned long long mb = 0;
     pid_t *p;
     int i;
@@ -28,7 +26,7 @@ int main( int argc, char *argv[] )
     setvbuf(stdout, NULL, _IONBF, 0);
 
     if(argc > 1)
-        max = atoll(argv[1]);
+        max_mem = atoll(argv[1]);
 
     if(argc > 2)
         max_proc = atoi(argv[2]);
@@ -36,7 +34,7 @@ int main( int argc, char *argv[] )
     p = (pid_t *)malloc(sizeof(pid_t) * max_proc);
     if (p == NULL) {
         perror("malloc failed");
-        return 255;
+        exit(EXIT_FAILURE);
     }
 
     signal(SIGINT, handle_sigint);
@@ -46,31 +44,38 @@ int main( int argc, char *argv[] )
 
       if(p[i]<0) {
         perror("fork failed");
-        exit(255);
-      } else if (p[i] == 0) {
+        exit(EXIT_FAILURE);
+      }
+
+      if (p[i] == 0) {
         pid_t pid = getpid();
 
-        while(mb <= max) {
+        while(mb < max_mem) {
           mb++;
-          buffer = realloc(buffer, alloc_chunk * mb);
-          if (buffer == NULL) {
-            perror("realloc failed");
-            break;
+          char *memory = (char *)malloc(alloc_chunk * mb);
+          if (memory == NULL) {
+            perror("malloc failed");
+            exit(EXIT_FAILURE);
           }
-
-          memset(buffer, 0, alloc_chunk * mb);
-          printf("%d(%d) allocated %llu MB \n", pid, i, mb);
+          memset(memory, 0, alloc_chunk * mb);
+          free(memory);
         }
 
-        exit(0);
+        printf("%d(%d) completed allocation of %llu MB\n", pid, i, mb);
+        sleep(10);
+
+        exit(EXIT_SUCCESS);
       }
+
+      sleep(0.1);
     }
 
     for (i = 0; i < max_proc; i++) {
         waitpid(p[i], &status, 0);
     }
 
-    free(buffer);
+    printf("All processes completed.\n");
+
     free(p);
     return 0;
 }
